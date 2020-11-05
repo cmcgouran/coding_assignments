7fe21eff44769a2d18ca4a9eb86ee1f4fcc62273
cmcgouran <71791380+cmcgouran@users.noreply.github.com>
2020-11-04 19:53:10 -0600

    Add files via upload

--- McGouran_assn4.sh
+++ McGouran_assn4.sh
@@ -7,16 +7,15 @@ awk 'NR==1 {$0=$0 OFS "Eligibility"; print; next }
 if ($2 >= 18) 
 $(NF+1)="eligible"
 else 
-$(NF+1)="ineligible"
-} 1' possible_voters.txt 
-
-#APPEND TO NEW FILE. 
->> results.txt
+$(NF+1)="naw"
+} 1' possible_voters.txt >> results.txt
 
 #ECHO FIRST 10 LINES/LAST 10 LINES. 
 
 head -n 10  results.txt
 tail -n 10 results.txt
 
+#DISPLAY HOW MANY ELIGIBLE VOTERS THERE ARE.
 
+grep -o  'eligible' results.txt | wc -l
 
